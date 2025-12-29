00ad943a90bffe3e348bf2da069272c3ce7ad718
Miguel Ángel González <mag.aragon@gmail.com>
2025-12-29 13:26:58 +0100

    feat(flyway): Added flyway dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.flywaydb:flyway-core")
 	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
